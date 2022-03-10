eb89aeadc5b89fac1e238b38f5a90ebbb0f01733
Vladimir Azarov <xazaro00@vutbr.cz>
2022-03-10 17:49:41 +0100

    added function names

---
+++ src/calc.py
@@ -0,0 +1,27 @@
+# All operations must respect these rools
+# 1) if number has more than 10 whole number places it should be written in
+# scientific notation. Max places after the dot in scientific notation == 4
+# 1_123_130_000_0 == 1.1231+e7
+# 2) Max decimal fractions before truncation == 10 (if number has only 1 
+# 3) Max numbers can be displayied on the calc is 11 
+# (so if the number is too big and wont fit (this could happen only if there will be too much 
+# decimal fractions), its decimal part will be cutted)
+
+
+def add(addent_1, addent_2):
+    pass
+def sub(minuend, subtrahend):
+    pass
+def mul(multiplicand, multiplier):
+    pass
+def div(divident, divisor):
+    pass
+def fact(num):
+    pass
+def exp(base, power):
+    pass
+def sqrt(radicand, index):
+    pass
+def mod(divident, divisor):
+    pass
+
