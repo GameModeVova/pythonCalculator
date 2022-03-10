b2512134ae8a1bc48f93bef26a31465ca4900804
Vladimir Azarov <xazaro00@vutbr.cz>
2022-03-10 18:07:02 +0100

    comments

--- src/calc.py
+++ src/calc.py
@@ -2,10 +2,11 @@
 # 1) if number has more than 10 whole number places it should be written in
 # scientific notation. Max places after the dot in scientific notation == 4
 # 1_123_130_000_0 == 1.1231+e7
-# 2) Max decimal fractions before truncation == 10 (if number has only 1 
-# 3) Max numbers can be displayied on the calc is 11 
+# 2) Max decimal fractions before truncation == 10 
+# 3) Max numbers can be displayied on the calculators display is 11 
 # (so if the number is too big and wont fit (this could happen only if there will be too much 
-# decimal fractions), its decimal part will be cutted)
+# decimal fractions because otherwise it will be displayied in scientific notation), 
+# its decimal part will be cutted and rounded)
 
 
 def add(addent_1, addent_2):
