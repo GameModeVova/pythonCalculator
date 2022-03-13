23537d356032d89fc65bc45ee51508ee5e033e7b
Vladimir Azarov <xazaro00@vutbr.cz>
2022-03-13 18:44:49 +0100

    fix tests

--- src/test_calc.py
+++ src/test_calc.py
@@ -7,39 +7,33 @@ class TestCase(unittest.TestCase):
     def test_add(self):
         self.assertEqual(calc.add(2, 3), 5)
         self.assertEqual(calc.add(1_000_000_000, 1), 1_000_000_001)
-        self.assertEqual(calc.add(1_000_000_000_0, 1), 1e10)
-        self.assertEqual(calc.add(1_000_000_000_0, 0), 1e10)
-        self.assertEqual(calc.add(1_000_000_000_0, 111_111_111_2), 1.1111e10)
-        self.assertEqual(calc.add(1.000_000_000_000_000_01 + 1), 1.000_000_000_0)
-        self.assertEqual(calc.add(1.000_000_000_000_000_01 + 1), 1.000_000_000_0)
+        self.assertEqual(calc.add(1, 0.000_000_000_1), 1.000_000_000_1)
 
     def test_sub(self):
         self.assertEqual(calc.sub(1, 1), 0)
-        self.assertEqual(calc.sub(1_000_000_000_000, 1_123), 1e11)
-        self.assertEqual(calc.sub(1_123, 1_000_000_000_000), -1e11)
-        self.assertEqual(calc.sub(1.0, 0.9999999999), 0.0000000001)
-        self.assertEqual(calc.sub(1.0, 0.99999999999), 0)
-        self.assertEqual(calc.sub(0.99999999999, 1.0), 0)
+        self.assertEqual(calc.sub(1.0, 0.999_999_999_9), 0.000_000_000_1)
+        self.assertEqual(calc.sub(0.999_999_999_9, 1.0), -0.000_000_000_1)
 
     def test_mul(self):
         self.assertEqual(calc.mul(0, 0), 0)
         self.assertEqual(calc.mul(2315523153, 0), 0)
-        self.assertEqual(calc.mul(5551234597, 5551234597), 3.0816e16)
-        self.assertEqual(calc.mul(5551234597, 2135), 1.1852e13)
         self.assertEqual(calc.mul(123489235, 0.53215231), 65_715_081.665)
         self.assertEqual(calc.mul(123489235, 0.523475), 64643527.292)
         self.assertEqual(calc.mul(123489235, 0.9), 111140311.5)
-        self.assertEqual(calc.div(0.0000000001, 0.0000000001), 0)
+        self.assertEqual(calc.div(0.0000000001, 0.0000000001), 1)
 
     def test_div(self):
         self.assertRaises(Exception, calc.div(231, 0))
         self.assertEqual(calc.div(0, 3421), 0)
-        self.assertEqual(calc.div(0.0000000001, 0.0000000001), 0)
-        self.assertEqual(calc.div(0.00001, 0.00001), 0.0000000001)
+        self.assertEqual(calc.div(0.000000001, 0.0000000001), 10)
+        self.assertEqual(calc.div(0.0000000001, 100), 0) 
+        self.assertEqual(calc.div(0.0000000001, 10), 0) 
+        self.assertEqual(calc.div(0.000000001, 123), 0) 
+        self.assertEqual(calc.div(0.000000001, 5.1), 0.0000000002) 
 
     def test_fact(self):
         self.assertEqual(calc.fact(0), 1)
-        self.assertEqual(calc.fact(14), 8.7178e10)
+        self.assertEqual(calc.fact(1), 1)
         self.assertEqual(calc.fact(13), 6_227_020_800)
         self.assertRaises(TypeError, calc.fact(2.2))
         self.assertRaises(ValueError, calc.fact(2.2))
@@ -47,10 +41,10 @@ class TestCase(unittest.TestCase):
 
     def test_exp(self):
         self.assertEqual(calc.exp(0, 324234), 0)
-        self.assertEqual(calc.exp(-1, 324234), 0)
-        self.assertEqual(calc.exp(-2.2, 239), -6.9027e81)
-        self.assertEqual(calc.exp(12, 232), 2.3445e261)
-        self.assertEqual(calc.exp(1.2, 232), 2.3445e18)
+        self.assertEqual(calc.exp(-1, 324234), -1)
+        self.assertEqual(calc.exp(2.3, 3), 12.167)
+        self.assertEqual(calc.exp(2.3, 6), 148.035889)
+        self.assertEqual(calc.exp(2.3, 9), 1801.1526615)
 
     def test_root(self):
         # n = -10
@@ -121,7 +115,6 @@ class TestCase(unittest.TestCase):
         self.assertEqual(calc.mod(-5.6, 2), 0.4) 
         self.assertEqual(calc.mod(-5.23, 2), 0.76) 
         self.assertEqual(calc.mod(12353251, 52311235), 12353251) 
-        self.assertEqual(calc.mod(1000000000000, 9999999999999), 1e12)
 
 if __name__ == "__main__":
     unittest.main()
