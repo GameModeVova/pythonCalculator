15d3349a28109c0401ea72cef70c961e6ba28fae
I-am-so-sleepy <ospaly@hotmail.com>
2022-03-14 17:37:16 +0100

    rounding
    added rounding to the tenth place

--- src/math_lib.py
+++ src/math_lib.py
@@ -2,25 +2,25 @@
 import math
 
 def add(first_value, second_value):
-    return(float(first_value)+float(second_value))
+    return(round(float(first_value)+float(second_value)), 10)
 
 def sub(first_value, second_value):
-    return(float(first_value)-float(second_value))
+    return(round(float(first_value)-float(second_value)), 10)
 
 def mul(first_value, second_value):
-    return(float(first_value)*float(second_value))
+    return(round(float(first_value)*float(second_value)), 10)
 
 def div(first_value, second_value):
-    return(float(first_value)/float(second_value))
+    return(round(float(first_value)/float(second_value)), 10)
 
 def exp(first_value, second_value): 
-    return(math.pow(float(first_value), float(second_value)))
+    return(round(math.pow(float(first_value), float(second_value))), 10)
 
 def root(first_value, second_value):
-    return(math.pow(float(second_value), 1/(float(first_value))))
+    return(round(math.pow(float(second_value), 1/(float(first_value)))), 10)
 
 def fact(first_value):
-    return(math.factorial(int(first_value)))
+    return(round(math.factorial(int(first_value))), 10)
 
 def mod(first_value, second_value):
-    return(float(first_value)%float(second_value))
+    return(round(float(first_value)%float(second_value)), 10)
