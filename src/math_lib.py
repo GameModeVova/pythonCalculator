3947899c10f9aff6f44d752cec22164c07fdf58b
Vladimir Azarov <xazaro00@vutbr.cz>
2022-03-14 20:12:34 +0100

    remove comments

--- src/math_lib.py
+++ src/math_lib.py
@@ -28,29 +28,6 @@ def root(first_value, second_value):
     except:
         Exception
 
-"""    complex_root_error = "ERROR MESSAGE" #change this to a meaningful error message
-    zero_root_error = "ERROR MESSAGE" #change this to a meaningful error message
-    negative_rooter_zero_root_error = "ERROR MESSAGE" #change this to a meaningful error message --- might also want to change the name
-
-    #CHECKING FOR ROOT BY ZERO
-    if second_value != 0:
-        #CHECKING FOR IMAGINARY NUMBER ERROR
-        if np.iscomplex(round(first_value**(1/second_value),10)):
-            return(complex_root_error)
-        else:
-            return(round(first_value**(1/second_value),10))
-    elif second_value == 0 and first_value == 1:
-        #CHECKING FOR IMAGINARY NUMBER ERROR
-        if np.iscomplex(round(first_value**(0),10)):
-            return(complex_root_error)
-        else:
-            return(round(first_value**(0),10))
-    elif first_value == 0 and second_value < 0:
-        return(negative_rooter_zero_root_error)
-    else:
-        return(zero_root_error)
-"""   
-
 def fact(first_value):
     non_integer_error = "YOU HAVE SINNED BY ATTEMPTING TO CALCULATE FACTORIAL OF A NON-INTEGER" #This error message should also be changed
 
