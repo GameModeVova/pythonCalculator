58e480984bf4ce965178993475c5c9446f3145e6
I-am-so-sleepy <ospaly@hotmail.com>
2022-03-14 18:08:38 +0100

    round fix plus some error checking
    fixed an issue with rounding
    added error checking for div --- now returns an error when attempting to divide by zero
    
    added error checking for mod --- now returns an error when trying to divide by zero
    
    added error checking for fact --- now returns an error when attempting to calculate factorial of a non-integer

--- src/math_lib.py
+++ src/math_lib.py
@@ -2,25 +2,42 @@
 import math
 
 def add(first_value, second_value):
-    return(round(float(first_value)+float(second_value)), 10)
+    return(round(float(first_value)+float(second_value),10))
 
 def sub(first_value, second_value):
-    return(round(float(first_value)-float(second_value)), 10)
+    return(round(float(first_value)-float(second_value),10))
 
 def mul(first_value, second_value):
-    return(round(float(first_value)*float(second_value)), 10)
+    return(round(float(first_value)*float(second_value),10))
 
 def div(first_value, second_value):
-    return(round(float(first_value)/float(second_value)), 10)
+    zero_div_error = "YOU HAVE COMMITED A SIN BY ATTEMPTING TO DIVIDE BY ZERO" #Change this error message to something actually meaningful
+    
+    #CHECKING FOR DIVISION BY ZERO
+    if second_value == 0:
+        return(zero_div_error)
+    else:
+        return(round(float(first_value)/float(second_value),10))
 
 def exp(first_value, second_value): 
-    return(round(math.pow(float(first_value), float(second_value))), 10)
+    return(round(math.pow(float(first_value), float(second_value)),10))
 
 def root(first_value, second_value):
-    return(round(math.pow(float(second_value), 1/(float(first_value)))), 10)
+    return(round(math.pow(float(second_value), 1/(float(first_value))),10))
 
 def fact(first_value):
-    return(round(math.factorial(int(first_value))), 10)
+    non_integer_error = "YOU HAVE SINNED BY ATTEMPTING TO CALCULATE FACTORIAL OF A NON-INTEGER" #This error message should also be changed
+
+    if first_value.is_integer():
+        return(round(math.factorial(int(first_value)),10))
+    else:
+        return(non_integer_error)
 
 def mod(first_value, second_value):
-    return(round(float(first_value)%float(second_value)), 10)
+    div_error = "YOU HAVE COMMITED A SIN BY ATTEMPTING TO DIVIDE BY ZERO" #Change this error message to something actually meaningful
+    
+    #CHECKING FOR DIVISION BY ZERO
+    if second_value == 0:
+        return(div_error)
+    else:
+        return(round(float(first_value)%float(second_value),10))
