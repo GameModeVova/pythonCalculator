3024c2a42aefecfa86d1622ddd45d0109e61df1d
Kriplingo <lucetta111@gmail.com>
2022-04-05 19:08:28 +0200

    Added mainloop back

--- src/calc.py
+++ src/calc.py
@@ -136,3 +136,5 @@ key_fac = tk.Button(window,text="!").grid(row=6,column=6,ipady =40,ipadx=40)
 key_delete = tk.Button(window,text="DELETE").grid(row=8,column=1,ipady =20,ipadx=40)
 key_clear = tk.Button(window,text="CLEAR").grid(row=8,column=2,ipady =20,ipadx=40)
 key_1_cache = tk.Button(window,text="CACHE").grid(row=8,column=3,ipady =20,ipadx=40)
+
+window.mainloop()
