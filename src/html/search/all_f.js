04af1cbef1c769561ac8f3e7d8f120f1f832a3a2
Vladimir Azarov <xazaro00@vutbr.cz>
2022-04-10 19:21:39 +0200

    Modified a little

--- src/html/search/all_f.js
+++ src/html/search/all_f.js
@@ -1,5 +1,14 @@
 var searchData=
 [
-  ['weight_0',['weight',['../namespacecalc.html#a103012df4b715f58a539b009a0b8b340',1,'calc']]],
-  ['window_1',['window',['../namespacecalc.html#a04a8a2bbfa9c15500892b8e5033d625b',1,'calc']]]
+  ['test_5fadd_0',['test_add',['../classtest__calc_1_1TestCase.html#aec5e4c0d54b0e55e74716ebd8f88ee7d',1,'test_calc::TestCase']]],
+  ['test_5fcalc_1',['test_calc',['../namespacetest__calc.html',1,'']]],
+  ['test_5fcalc_2epy_2',['test_calc.py',['../test__calc_8py.html',1,'']]],
+  ['test_5fdiv_3',['test_div',['../classtest__calc_1_1TestCase.html#a3abca85f0dbe6c1a7d2ef53808854c99',1,'test_calc::TestCase']]],
+  ['test_5fexp_4',['test_exp',['../classtest__calc_1_1TestCase.html#ae77f9497d818b84aefb673d03fc00446',1,'test_calc::TestCase']]],
+  ['test_5ffact_5',['test_fact',['../classtest__calc_1_1TestCase.html#a52bbb5343fb77a0d72b0ecb03750a988',1,'test_calc::TestCase']]],
+  ['test_5fmod_6',['test_mod',['../classtest__calc_1_1TestCase.html#abfebaca21569b8b14f1de5c325edfb6c',1,'test_calc::TestCase']]],
+  ['test_5fmul_7',['test_mul',['../classtest__calc_1_1TestCase.html#afb783b26fd9dab1ac3817fab56fd107c',1,'test_calc::TestCase']]],
+  ['test_5froot_8',['test_root',['../classtest__calc_1_1TestCase.html#a07b66ba337b41595ca788e2b07f54e6d',1,'test_calc::TestCase']]],
+  ['test_5fsub_9',['test_sub',['../classtest__calc_1_1TestCase.html#a432b8da2fd162b2de4ad004f5aa00e89',1,'test_calc::TestCase']]],
+  ['testcase_10',['TestCase',['../classtest__calc_1_1TestCase.html',1,'test_calc']]]
 ];
